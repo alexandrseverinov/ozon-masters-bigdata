6e354b799af4bf8e8480a9639cee5fe386b56531
Severinov Aleksandr <aseverinov@ozon.ru>
2019-10-27 18:41:36 +0300

    columns for prediction fixed

--- projects/1/predict.py
+++ projects/1/predict.py
@@ -7,9 +7,6 @@ import logging
 from joblib import load
 import pandas as pd
 
-sys.path.append('.')
-from model import fields
-
 
 # Init the logger
 logging.basicConfig(level=logging.DEBUG)
@@ -20,14 +17,28 @@ logging.info("ARGS {}".format(sys.argv[1:]))
 # load the model
 model = load("1.joblib")
 
+numeric_features = ["if" + str(i) for i in range(1, 14)]
+categorical_features_full = ["cf" + str(i) for i in range(1, 27)] + ["day_number"]
+fields_full = ["id"] + numeric_features + categorical_features_full
+
+to_drop_categorical = [20, 1, 22, 10, 21, 12, 11, 23]
+categorical_features = (
+    [
+        "cf" + str(i)
+        for i in range(1, 27) if i not in to_drop_categorical
+    ] + ["day_number"]
+)
+
+fields = ["id"] + numeric_features + categorical_features
+
 # read and inference
 read_opts = dict(
-        sep="\t", names=fields, index_col=False, header=None,
+        sep="\t", names=fields_full, index_col=False, header=None,
         iterator=True, chunksize=100
 )
 
 for df in pd.read_csv(sys.stdin, **read_opts):
-    pred = model.predict_proba(df)
+    pred = model.predict_proba(df[fields])
     print(
         "\n".join(
             [f"{id},{prediction}"
